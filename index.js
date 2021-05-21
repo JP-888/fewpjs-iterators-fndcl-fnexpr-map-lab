6c17fb40d55bbedb6c89216c5aa1f56cd2525bed
JP <jpsimshed@gmail.com>
2021-05-21 20:40:59 +1000

    Completed lab

--- index.js
+++ index.js
@@ -11,6 +11,8 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
-}
+const titleCased = () => tutorials.map(sentence => {
+    return sentence.split(' ').map(word => {
+      return word[0].toUpperCase() + word.slice(1)
+    }).join(' ')
+})
